df349a547107ef97a574a8b8030940822010cbaa
Zofia Perzynska <zosiaperzynskaa@wp.pl>
2023-03-19 12:47:30 +0100

    Added mui date-pickers into project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
+import Form from "./components/Form/Form";
 
 function App() {
-  return <div></div>;
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Form />
+    </LocalizationProvider>
+  );
 }
 
 export default App;
